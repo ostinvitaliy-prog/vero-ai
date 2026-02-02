c706cd4e925229b6788b1325dedc421ce6ef3088
ostinvitaliy-prog <ostin.vitaliy@gmail.com>
2026-02-02 20:20:21 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+API_TOKEN = '8050168002:AAEnS0NsuVn4-_WZbOLTpluPDP8BCqF3CUQ'
+ROUTEL_API_KEY = 's2_4b5416fae8a44bc7b97dd7bd65bb0f3b'
+BASE_URL = "https://routellm.abacus.ai/v1"
+DB_NAME = 'vero.db'
+NEWS_CHECK_INTERVAL = 600  # 10 минут
+MIN_NEWS_SCORE = 7         # Порог важности
