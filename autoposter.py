b67170ec53462679a7a0f90b9122e1fa7d7ab354
ostinvitaliy-prog <ostin.vitaliy@gmail.com>
2026-02-02 21:21:13 +0100

    Update autoposter.py

--- autoposter.py
+++ autoposter.py
@@ -16,11 +16,15 @@ async def start_autoposter(bot):
                     res = await ai.analyze_and_style_news(entry.title, entry.description)
                     
                     if res and res.get('score', 0) >= MIN_NEWS_SCORE:
-                        db.save_news(res['content'], entry.link, res['score'])
-                        users = db.get_all_users()
-                        for u_id in users:
-                            try:
-                                await bot.send_message(u_id, f"{res['content']}\n\n🔗 <a href='{entry.link}'>Источник</a>", parse_mode="HTML", disable_web_page_preview=True)
-                            except: pass
+                        db.save_news(res['ru'], res['en'], res['es'], res['de'], entry.link, res['score'])
+                        
+                        for lang in ['ru', 'en', 'es', 'de']:
+                            users = db.get_users_by_lang(lang)
+                            for u_id in users:
+                                try:
+                                    await bot.send_message(u_id, f"{res[lang]}\n\n🔗 <a href='{entry.link}'>Source</a>", parse_mode="HTML", disable_web_page_preview=True)
+                                except: pass
+            
             await asyncio.sleep(NEWS_CHECK_INTERVAL)
-        except: await asyncio.sleep(60)
+        except: 
+            await asyncio.sleep(60)
