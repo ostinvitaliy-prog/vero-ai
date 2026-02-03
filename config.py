45eac32e6f633c3b4c1f950839ed60194825d01e
ostinvitaliy-prog <ostin.vitaliy@gmail.com>
2026-02-03 11:53:12 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
 import os
 
-# Твои боевые ключи
 BOT_TOKEN = "8050168002:AAEnS0NsuVn4-_WZbOLTpluPDP8BCqF3CUQ"
-ROUTEL_API_KEY = "abacus_v1_5e0e8a8e8e8e4e4e8e8e8e8e8e8e8e8e"
-
-# Настройки системы
+ROUTEL_API_KEY = "s2_cd95ee23222c4b6ca384311db784c514"
 BASE_URL = "https://routellm.abacus.ai/v1"
-NEWS_CHECK_INTERVAL = 600  # 10 минут
-MIN_NEWS_SCORE = 7
-DB_NAME = "vero.db"
