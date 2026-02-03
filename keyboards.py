f4fbf87286b672096612ccb640199577f795757d
ostinvitaliy-prog <ostin.vitaliy@gmail.com>
2026-02-03 09:01:18 +0100

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,8 +0,0 @@
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from aiogram import types
-
-def main_menu():
-    builder = ReplyKeyboardBuilder()
-    builder.row(types.KeyboardButton(text="📊 Live Report"), types.KeyboardButton(text="💎 VERO Exclusive"))
-    builder.row(types.KeyboardButton(text="📢 Free Feed"), types.KeyboardButton(text="👤 My Profile"))
-    return builder.as_markup(resize_keyboard=True)
