8d5e87187b61126f9ac0eafe9939125674abb05d
ostinvitaliy-prog <ostin.vitaliy@gmail.com>
2026-02-08 10:26:11 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,24 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
-import { CronService } from './cron/cron.service';
-import { RssService } from './rss/rss.service';
-import { TelegramService } from './telegram/telegram.service';
+import { AppService } from './app.service';
 import { AiService } from './ai/ai.service';
+import { TelegramService } from './telegram/telegram.service';
 import { DatabaseService } from './database/database.service';
+import { RssService } from './rss/rss.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    ScheduleModule.forRoot()
+    ScheduleModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [
-    CronService,
-    RssService,
-    TelegramService,
-    AiService,
-    DatabaseService
+    AppService, 
+    AiService, 
+    TelegramService, 
+    DatabaseService, 
+    RssService
   ],
 })
 export class AppModule {}
